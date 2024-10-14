5ebfbe126a6d9906c06edce088a78319959b5343
Paulo <paulossjunior@gmail.com>
2024-10-14 18:48:48 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a operação desejada: ");
+            Console.WriteLine("Soma");
+            Console.WriteLine("Subtração");
+            Console.WriteLine("Multiplicação");
+            Console.WriteLine("Divisão");
+            Console.WriteLine("Sair");
+
+            string operacao = Console.ReadLine();
+            Console.WriteLine("Digite o Primeiro Valor:");
+
+            string valorInformado = Console.ReadLine();
+            int primeiroValor = Convert.ToInt32(valorInformado);
+
+            Console.WriteLine("Digite o Segundo Valor:");
+            valorInformado = Console.ReadLine();
+            int segundoValor = Convert.ToInt32(valorInformado);
+            int resultado = 0;
+
+            switch (operacao)
+            {
+                case "Soma":
+                    resultado = primeiroValor + segundoValor;
+                    break;
+
+                case "Subtração":
+                    resultado = primeiroValor - segundoValor;
+                    break;
+
+                case "Multiplicação":
+                    resultado = primeiroValor * segundoValor;
+                    break;
+
+                case "Divisão":
+                    resultado = primeiroValor / segundoValor;
+                    break;
+
+                default:
+                    Console.WriteLine("Opção Invalida");
+                    break;
+            }
+
+            Console.WriteLine($"Resultado: {resultado}");
+
+        }
+    }
+}
